0332c462d01bca0937878bf785f8ce7aa65ecbb9
RubenZubicoa <rubenzubicoatic@gmail.com>
2016-10-24 13:17:32 +0200

    1

--- src/main/java/org/classapp/backend/data/impl/HibernateUserDAO.java
+++ src/main/java/org/classapp/backend/data/impl/HibernateUserDAO.java
@@ -1,5 +1,87 @@
 package org.classapp.backend.data.impl;
 
-public class HibernateUserDAO {
+import java.util.List;
+
+import org.classapp.backend.data.dao.UserDAO;
+import org.classapp.backend.data.HibernateSession;
+import org.classapp.backend.data.model.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+public class HibernateUserDAO implements UserDAO {
+	
+	
+	public User selectById(Long id) {
+		SessionFactory sessionFactory = HibernateSession.getSessionFactory();
+		Session session = sessionFactory.openSession();
+		User customer = (User) session.get(User.class, id);
+		session.close();
+		return customer;
+	}
+
+	/*
+	 * retrieves all customers from db
+	 * 
+	 * @return List of customers
+	 */
+	public List<User> selectAll() {
+		SessionFactory sessionFactory = HibernateSession.getSessionFactory();
+		Session session = sessionFactory.openSession();
+		List<User> customers = session.createCriteria(User.class).list();
+		session.close();
+		return customers;
+	}
+
+	/*
+	 * inserts a new customer in database retrieves generated id and sets to
+	 * customer instance
+	 * 
+	 * @param new customer
+	 */
+	public void insert(User user) {
+		SessionFactory sessionFactory = HibernateSession.getSessionFactory();
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+		Long id =  (Long) session.save(user);
+		user.setId(id);
+		session.getTransaction().commit();
+		session.close();
+	}
+	/*
+	 * updates customer
+	 * @param customer to update
+	 */
+	public void update(User customer) {
+		
+		SessionFactory sessionFactory = HibernateSession.getSessionFactory();
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+		session.merge(customer);
+		session.getTransaction().commit();
+		session.close();
+		
+	}
+
+	/*
+	 * delete given customer
+	 * 
+	 * @param customer to delete
+	 */
+	public void delete(User customer) {
+		SessionFactory sessionFactory = HibernateSession.getSessionFactory();
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+		session.delete(customer);
+		session.getTransaction().commit();
+		session.close();
+	}
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
--- src/main/java/org/classapp/backend/data/model/User.java
+++ src/main/java/org/classapp/backend/data/model/User.java
@@ -1,13 +1,96 @@
 package org.classapp.backend.data.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+
+
 public class User {
+	Long id;
+	String username;
+	String password;
+	String email;
+	Avatar avatar;
+	public Avatar getAvatar() {
+		return avatar;
+	}
+
+
+	public void setAvatar(Avatar avatar) {
+		this.avatar = avatar;
+	}
+
+
+	
+	
 	
 	public User(){}
+<<<<<<< HEAD
 	
 	
 	public User(String name){
 		
+=======
+
+
+	public User(Long id, String username, String password, String email) {
+		super();
+		this.id =  id;
+		this.username = username;
+		this.password = password;
+		this.email = email;
+>>>>>>> refs/remotes/origin/BRANCH_ADRIAN
 	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id2) {
+		id = id2;
+	}
+
+
+	public String getUsername() {
+		return username;
+	}
+
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	
+	
+	
+
 	
 	
 
