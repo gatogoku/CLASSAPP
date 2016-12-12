d316a2e1a5194cf78e9aa33c2ed9287802cbf99c
jotesan <jotesan357@gmail.com>
2016-12-12 14:09:11 +0100

    More changes

--- src/main/java/org/classapp/backend/data/impl/HibernateRoleDao.java
+++ src/main/java/org/classapp/backend/data/impl/HibernateRoleDao.java
@@ -3,6 +3,6 @@ package org.classapp.backend.data.impl;
 import org.classapp.backend.data.dao.RoleDAO;
 import org.classapp.backend.data.model.Role;
 
-public class HibernateRoleDAO extends GenericDAOHibernate<Role> implements RoleDAO {
+public class HibernateRoleDao extends GenericDAOHibernate<Role> implements RoleDAO {
 
 }
